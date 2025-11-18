447e3a006dd335ed86cc6ffeef14e03cf3c3471c
Richard Kováč <living.cheap.american@gmail.com>
2025-11-18 11:42:06 +0000

    File changes

--- functions/analyzujDomyBatch.ts
+++ functions/analyzujDomyBatch.ts
@@ -109,9 +109,7 @@ Deno.serve(async (req) => {
 
 // Background processing function - beží na serveri nezávisle
 async function runBatchAnalysisInBackground(documents, userId) {
-    const APP_ID = Deno.env.get('BASE44_APP_ID');
-    const SERVICE_KEY = Deno.env.get('BASE44_SERVICE_ROLE_KEY');
-    const serviceClient = createServiceRoleClient(APP_ID, SERVICE_KEY);
+    const serviceClient = createServiceRoleClient();
 
     for (let i = 0; i < documents.length; i++) {
         try {
