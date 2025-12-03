3cab97bc362b70d4c0c5e4d970cae147c0905984
Richard Kováč <living.cheap.american@gmail.com>
2025-12-03 14:56:01 +0000

    File changes

--- src/components/FlyingAnimation.jsx
+++ src/components/FlyingAnimation.jsx
@@ -10,7 +10,7 @@ const FLYING_ITEMS = {
   elektro: "💡", // žiarovka
   voda: "🚰", // potrubie s vodou
   sanita: "🚿🪥🚽", // sanita
-  bojler: "🔲", // bojler - elektrický bojler
+  bojler: "🏠🔥", // bojler - ohrievač vody
   klimatizacia: "❄️", // klimatizácia
   rekuperacia: "🌀", // ventilátor
   siete: "🚜", // bager
