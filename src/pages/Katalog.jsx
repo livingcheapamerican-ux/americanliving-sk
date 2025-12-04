81d2aab5c3e0f48b0784571a334e78a36c32182c
Richard Kováč <living.cheap.american@gmail.com>
2025-12-04 14:24:32 +0000

    File changes

--- src/pages/Katalog.jsx
+++ src/pages/Katalog.jsx
@@ -17,15 +17,7 @@ import { motion } from "framer-motion";
 export default function Katalog() {
   const location = useLocation();
   const navigate = useNavigate();
-  const [imageToggle, setImageToggle] = useState(false);
 
-  // Striedanie obrázkov každé 2 sekundy
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setImageToggle(prev => !prev);
-    }, 4000);
-    return () => clearInterval(interval);
-  }, []);
   
   // Funkcia na parsovanie URL parametrov
   const getInitialFilters = () => {
