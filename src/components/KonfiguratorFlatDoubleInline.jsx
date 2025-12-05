a17ace24fa33b2600f2910d2393c901b9afa9fe8
Richard Kováč <living.cheap.american@gmail.com>
2025-12-05 00:01:49 +0000

    File changes

--- src/components/KonfiguratorFlatDoubleInline.jsx
+++ src/components/KonfiguratorFlatDoubleInline.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo, useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { Button } from "@/components/ui/button";
