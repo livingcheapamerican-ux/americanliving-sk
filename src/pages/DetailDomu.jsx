c98a6eb7e9c104b8bce9727d29d50904159d4059
Richard Kováč <living.cheap.american@gmail.com>
2025-12-05 17:10:35 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -1132,7 +1132,7 @@ export default function DetailDomu() {
 
             {/* Floating Konfigurátor Súhrn - len pre Prosto House Flat Double - hned pod zakladnu konfiguraciu */}
             {isProstoHouse && dom.nazov?.includes("Flat Double") && dom.zastavana_plocha === 142 && (
-              <div className="lg:sticky lg:top-24 z-10 self-start max-h-[calc(100vh-120px)] overflow-hidden">
+              <div className="lg:sticky lg:top-20 z-10 self-start" style={{ position: 'sticky', top: '80px' }}>
                 <KonfiguratorFlatDoubleInline 
                   dom={dom}
                   montazHolodomu={montazHolodomu}
