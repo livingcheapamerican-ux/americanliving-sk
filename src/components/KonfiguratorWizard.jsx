aa33edb4d402bed4594ae9161843841c7e93c9e7
Richard Kováč <living.cheap.american@gmail.com>
2025-12-06 10:16:58 +0000

    File changes

--- src/components/KonfiguratorWizard.jsx
+++ src/components/KonfiguratorWizard.jsx
@@ -13,6 +13,7 @@ import KonfiguratorFaza1HrubaStavba from "../components/KonfiguratorFaza1HrubaSt
 import KonfiguratorFlatDoubleInline from "../components/KonfiguratorFlatDoubleInline";
 import KonfiguratorFjord from "../components/KonfiguratorFjord";
 import KonfiguratorNord from "../components/KonfiguratorNord";
+import KonfiguratorProstoHouse from "../components/KonfiguratorProstoHouse";
 import { useLanguage } from "./LanguageContext";
 
 // Krok 0: Výber typu stavby
@@ -1077,8 +1078,8 @@ export default function KonfiguratorWizard({
             showOnlyPhase="docs"
             typStavby={typStavby}
           />
-        ) : useFjordPrices ? (
-          <KonfiguratorFjord
+        ) : useProstoHousePrices ? (
+          <KonfiguratorProstoHouse
             dom={dom}
             onReset={handleFullReset}
             montazHolodomu={montazHolodomu}
@@ -1139,8 +1140,73 @@ export default function KonfiguratorWizard({
             showOnlyPhase="docs"
             typStavby={typStavby}
           />
-        ) : (
-          <KonfiguratorFlatDoubleInline
+        ) : useFjordPrices ? (
+          <KonfiguratorFjord
+            dom={dom}
+            onReset={handleFullReset}
+            montazHolodomu={montazHolodomu}
+            setMontazHolodomu={setMontazHolodomu}
+            izolaciaNavysenie={izolaciaNavysenie}
+            setIzolaciaNavysenie={setIzolaciaNavysenie}
+            zaklady={zaklady}
+            setZaklady={setZaklady}
+            vstupneDvere={vstupneDvere}
+            setVstupneDvere={setVstupneDvere}
+            elektroinstalacia={elektroinstalacia}
+            setElektroinstalacia={setElektroinstalacia}
+            vodaKanalizacia={vodaKanalizacia}
+            setVodaKanalizacia={setVodaKanalizacia}
+            sanitaKomplet={sanitaKomplet}
+            setSanitaKomplet={setSanitaKomplet}
+            bojler={bojler}
+            setBojler={setBojler}
+            tepelneCerpadlo={tepelneCerpadlo}
+            setTepelneCerpadlo={setTepelneCerpadlo}
+            rekuperacia={rekuperacia}
+            setRekuperacia={setRekuperacia}
+            pripojkaSiete={pripojkaSiete}
+            setPripojkaSiete={setPripojkaSiete}
+            stresneOkno={stresneOkno}
+            setStresneOkno={setStresneOkno}
+            bocneOknoFixne={bocneOknoFixne}
+            setBocneOknoFixne={setBocneOknoFixne}
+            bocneOknoVyklopne90={bocneOknoVyklopne90}
+            setBocneOknoVyklopne90={setBocneOknoVyklopne90}
+            bocneOknoVyklopne55={bocneOknoVyklopne55}
+            setBocneOknoVyklopne55={setBocneOknoVyklopne55}
+            povrchokaOkien={povrchokaOkien}
+            setPovrchokaOkien={setPovrchokaOkien}
+            tonovaneSkla={tonovaneSkla}
+            setTonovaneSkla={setTonovaneSkla}
+            vonkajsiaFasada={vonkajsiaFasada}
+            setVonkajsiaFasada={setVonkajsiaFasada}
+            interierFinis={interierFinis}
+            setInterierFinis={setInterierFinis}
+            vnutornePodlahy={vnutornePodlahy}
+            setVnutornePodlahy={setVnutornePodlahy}
+            podlahovVykurovanie={podlahovVykurovanie}
+            setPodlahovVykurovanie={setPodlahovVykurovanie}
+            interieroveDvere={interieroveDvere}
+            setInterieroveDvere={setInterieroveDvere}
+            pergola={pergola}
+            setPergola={setPergola}
+            inziniering={inziniering}
+            setInziniering={setInziniering}
+            projektA0={projektA0}
+            setProjektA0={setProjektA0}
+            revizna={revizna}
+            setRevizna={setRevizna}
+            doprava={doprava}
+            setDoprava={setDoprava}
+            showOnlySummary={false}
+            showOnlyPhase="kluc"
+            typStavby={typStavby}
+          />
+        ) : useFjordPrices ? (
+          <KonfiguratorFjord
+            dom={dom}
+            onReset={handleFullReset}
+            montazHolodomu={montazHolodomu}
             dom={dom}
             onReset={handleFullReset}
             montazHolodomu={montazHolodomu}
