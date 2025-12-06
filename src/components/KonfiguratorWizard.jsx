81f0c8929823a112410186a5fb6c01e8ca46a72e
Richard Kováč <living.cheap.american@gmail.com>
2025-12-06 10:17:47 +0000

    File changes

--- src/components/KonfiguratorWizard.jsx
+++ src/components/KonfiguratorWizard.jsx
@@ -702,7 +702,69 @@ export default function KonfiguratorWizard({
           />
         );
       case 3:
-        return useFlat15Prices ? (
+        return useProstoHousePrices ? (
+          <KonfiguratorProstoHouse
+            dom={dom}
+            onReset={handleFullReset}
+            montazHolodomu={montazHolodomu}
+            setMontazHolodomu={setMontazHolodomu}
+            izolaciaNavysenie={izolaciaNavysenie}
+            setIzolaciaNavysenie={setIzolaciaNavysenie}
+            zaklady={zaklady}
+            setZaklady={setZaklady}
+            vstupneDvere={vstupneDvere}
+            setVstupneDvere={setVstupneDvere}
+            elektroinstalacia={elektroinstalacia}
+            setElektroinstalacia={setElektroinstalacia}
+            vodaKanalizacia={vodaKanalizacia}
+            setVodaKanalizacia={setVodaKanalizacia}
+            sanitaKomplet={sanitaKomplet}
+            setSanitaKomplet={setSanitaKomplet}
+            bojler={bojler}
+            setBojler={setBojler}
+            tepelneCerpadlo={tepelneCerpadlo}
+            setTepelneCerpadlo={setTepelneCerpadlo}
+            rekuperacia={rekuperacia}
+            setRekuperacia={setRekuperacia}
+            pripojkaSiete={pripojkaSiete}
+            setPripojkaSiete={setPripojkaSiete}
+            stresneOkno={stresneOkno}
+            setStresneOkno={setStresneOkno}
+            bocneOknoFixne={bocneOknoFixne}
+            setBocneOknoFixne={setBocneOknoFixne}
+            bocneOknoVyklopne90={bocneOknoVyklopne90}
+            setBocneOknoVyklopne90={setBocneOknoVyklopne90}
+            bocneOknoVyklopne55={bocneOknoVyklopne55}
+            setBocneOknoVyklopne55={setBocneOknoVyklopne55}
+            povrchokaOkien={povrchokaOkien}
+            setPovrchokaOkien={setPovrchokaOkien}
+            tonovaneSkla={tonovaneSkla}
+            setTonovaneSkla={setTonovaneSkla}
+            vonkajsiaFasada={vonkajsiaFasada}
+            setVonkajsiaFasada={setVonkajsiaFasada}
+            interierFinis={interierFinis}
+            setInterierFinis={setInterierFinis}
+            vnutornePodlahy={vnutornePodlahy}
+            setVnutornePodlahy={setVnutornePodlahy}
+            podlahovVykurovanie={podlahovVykurovanie}
+            setPodlahovVykurovanie={setPodlahovVykurovanie}
+            interieroveDvere={interieroveDvere}
+            setInterieroveDvere={setInterieroveDvere}
+            pergola={pergola}
+            setPergola={setPergola}
+            inziniering={inziniering}
+            setInziniering={setInziniering}
+            projektA0={projektA0}
+            setProjektA0={setProjektA0}
+            revizna={revizna}
+            setRevizna={setRevizna}
+            doprava={doprava}
+            setDoprava={setDoprava}
+            showOnlySummary={false}
+            showOnlyPhase="kluc"
+            typStavby={typStavby}
+          />
+        ) : useFlat15Prices ? (
           <KonfiguratorFlatDoubleInline
             dom={dom}
             onReset={handleFullReset}
@@ -953,7 +1015,69 @@ export default function KonfiguratorWizard({
           />
         );
       case 4:
-        return useFlat15Prices ? (
+        return useProstoHousePrices ? (
+          <KonfiguratorProstoHouse
+            dom={dom}
+            onReset={handleFullReset}
+            montazHolodomu={montazHolodomu}
+            setMontazHolodomu={setMontazHolodomu}
+            izolaciaNavysenie={izolaciaNavysenie}
+            setIzolaciaNavysenie={setIzolaciaNavysenie}
+            zaklady={zaklady}
+            setZaklady={setZaklady}
+            vstupneDvere={vstupneDvere}
+            setVstupneDvere={setVstupneDvere}
+            elektroinstalacia={elektroinstalacia}
+            setElektroinstalacia={setElektroinstalacia}
+            vodaKanalizacia={vodaKanalizacia}
+            setVodaKanalizacia={setVodaKanalizacia}
+            sanitaKomplet={sanitaKomplet}
+            setSanitaKomplet={setSanitaKomplet}
+            bojler={bojler}
+            setBojler={setBojler}
+            tepelneCerpadlo={tepelneCerpadlo}
+            setTepelneCerpadlo={setTepelneCerpadlo}
+            rekuperacia={rekuperacia}
+            setRekuperacia={setRekuperacia}
+            pripojkaSiete={pripojkaSiete}
+            setPripojkaSiete={setPripojkaSiete}
+            stresneOkno={stresneOkno}
+            setStresneOkno={setStresneOkno}
+            bocneOknoFixne={bocneOknoFixne}
+            setBocneOknoFixne={setBocneOknoFixne}
+            bocneOknoVyklopne90={bocneOknoVyklopne90}
+            setBocneOknoVyklopne90={setBocneOknoVyklopne90}
+            bocneOknoVyklopne55={bocneOknoVyklopne55}
+            setBocneOknoVyklopne55={setBocneOknoVyklopne55}
+            povrchokaOkien={povrchokaOkien}
+            setPovrchokaOkien={setPovrchokaOkien}
+            tonovaneSkla={tonovaneSkla}
+            setTonovaneSkla={setTonovaneSkla}
+            vonkajsiaFasada={vonkajsiaFasada}
+            setVonkajsiaFasada={setVonkajsiaFasada}
+            interierFinis={interierFinis}
+            setInterierFinis={setInterierFinis}
+            vnutornePodlahy={vnutornePodlahy}
+            setVnutornePodlahy={setVnutornePodlahy}
+            podlahovVykurovanie={podlahovVykurovanie}
+            setPodlahovVykurovanie={setPodlahovVykurovanie}
+            interieroveDvere={interieroveDvere}
+            setInterieroveDvere={setInterieroveDvere}
+            pergola={pergola}
+            setPergola={setPergola}
+            inziniering={inziniering}
+            setInziniering={setInziniering}
+            projektA0={projektA0}
+            setProjektA0={setProjektA0}
+            revizna={revizna}
+            setRevizna={setRevizna}
+            doprava={doprava}
+            setDoprava={setDoprava}
+            showOnlySummary={false}
+            showOnlyPhase="docs"
+            typStavby={typStavby}
+          />
+        ) : useFlat15Prices ? (
           <KonfiguratorFlatDoubleInline
             dom={dom}
             onReset={handleFullReset}
