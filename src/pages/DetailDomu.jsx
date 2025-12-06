2c0866ec64b3b731317f0ed160ebdfa7f5735fee
Richard Kováč <living.cheap.american@gmail.com>
2025-12-06 23:56:29 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -1534,6 +1534,7 @@ export default function DetailDomu() {
                   </div>
                 </Card>
               </div>
+              </div>
             )}
 
             {/* Floating panel s cenami pre Flat 1,5 - pravá strana */}
