4797e2d0502f0edeff48f4be9df807aa61e81580
Richard Kováč <living.cheap.american@gmail.com>
2025-12-07 16:11:23 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -649,6 +649,8 @@ export default function DetailDomu() {
                 key={wizardKey}
                 dom={dom}
                 useFlat15Prices={true}
+                typStavby={typStavby}
+                setTypStavby={setTypStavby}
                 montazHolodomu={montazHolodomu}
                 setMontazHolodomu={setMontazHolodomu}
                 izolaciaNavysenie={izolaciaNavysenie}
