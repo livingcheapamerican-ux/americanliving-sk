b36ad904a7aa12da2ff1c7c123a83ae788bbe1b4
Richard Kováč <living.cheap.american@gmail.com>
2025-12-08 20:17:30 +0000

    File changes

--- src/pages/Katalog.jsx
+++ src/pages/Katalog.jsx
@@ -544,7 +544,7 @@ export default function Katalog() {
                     transition={{ delay: index * 0.05 }}>
 
                       <Card className={`group overflow-hidden hover:shadow-2xl transition-all duration-300 hover:-translate-y-1 sm:hover:-translate-y-2 bg-white ${jeVybrany ? 'ring-2 ring-primary' : ''} ${dom.verejny === false ? 'opacity-60' : ''}`}>
-                        <div className="relative h-28 sm:h-56 overflow-hidden">
+                        <div className="relative h-28 sm:h-72 overflow-hidden">
                           <Link to={`${createPageUrl("DetailDomu")}?id=${dom.id}&return=${encodeURIComponent(location.pathname + location.search)}`}>
                             {dom.hlavny_obrazok ? (
                               <img
