4cd14bde54eae67b6f1b1eb3e3c67490a869a2c3
Richard Kováč <living.cheap.american@gmail.com>
2025-12-17 18:48:07 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -2227,91 +2227,14 @@ export default function DetailDomu() {
             </div>
             )}
 
-                {/* Štandardná výbava pre JAK Modules - presunute z pravej strany */}
-                  setPredajNehnutelnosti={setPredajNehnutelnosti}
-                  hladaniePozemku={hladaniePozemku}
-                  setHladaniePozemku={setHladaniePozemku}
-                  financneSluzby={financneSluzby}
-                  setFinancneSluzby={setFinancneSluzby}
-                  typStavby={typStavby}
-                  setTypStavby={setTypStavby}
-                  montazHolodomu={montazHolodomu}
-                  setMontazHolodomu={setMontazHolodomu}
-                  izolaciaNavysenie={izolaciaNavysenie}
-                  setIzolaciaNavysenie={setIzolaciaNavysenie}
-                  zaklady={zaklady}
-                  setZaklady={setZaklady}
-                  predlzenie={predlzenie}
-                  setPredlzenie={setPredlzenie}
-                  vstupneDvere={vstupneDvere}
-                  setVstupneDvere={setVstupneDvere}
-                  elektroinstalacia={elektroinstalacia}
-                  setElektroinstalacia={setElektroinstalacia}
-                  vodaKanalizacia={vodaKanalizacia}
-                  setVodaKanalizacia={setVodaKanalizacia}
-                  sanitaKomplet={sanitaKomplet}
-                  setSanitaKomplet={setSanitaKomplet}
-                  bojler={bojler}
-                  setBojler={setBojler}
-                  tepelneCerpadlo={tepelneCerpadlo}
-                  setTepelneCerpadlo={setTepelneCerpadlo}
-                  rekuperacia={rekuperacia}
-                  setRekuperacia={setRekuperacia}
-                  pripojkaSiete={pripojkaSiete}
-                  setPripojkaSiete={setPripojkaSiete}
-                  stresneOkno={stresneOkno}
-                  setStresneOkno={setStresneOkno}
-                  bocneOknoFixne={bocneOknoFixne}
-                  setBocneOknoFixne={setBocneOknoFixne}
-                  bocneOknoVyklopne90={bocneOknoVyklopne90}
-                  setBocneOknoVyklopne90={setBocneOknoVyklopne90}
-                  bocneOknoVyklopne55={bocneOknoVyklopne55}
-                  setBocneOknoVyklopne55={setBocneOknoVyklopne55}
-                  povrchokaOkien={povrchokaOkien}
-                  setPovrchokaOkien={setPovrchokaOkien}
-                  tonovaneSkla={tonovaneSkla}
-                  setTonovaneSkla={setTonovaneSkla}
-                  vonkajsiaFasada={vonkajsiaFasada}
-                  setVonkajsiaFasada={setVonkajsiaFasada}
-                  interierFinis={interierFinis}
-                  setInterierFinis={setInterierFinis}
-                  vnutornePodlahy={vnutornePodlahy}
-                  setVnutornePodlahy={setVnutornePodlahy}
-                  podlahovVykurovanie={podlahovVykurovanie}
-                  setPodlahovVykurovanie={setPodlahovVykurovanie}
-                  interieroveDvere={interieroveDvere}
-                  setInterieroveDvere={setInterieroveDvere}
-                  pergola={pergola}
-                  setPergola={setPergola}
-                  inziniering={inziniering}
-                  setInziniering={setInziniering}
-                  projektA0={projektA0}
-                  setProjektA0={setProjektA0}
-                  revizna={revizna}
-                  setRevizna={setRevizna}
-                  doprava={doprava}
-                  setDoprava={setDoprava}
-                  showOnlySummary={true}
-                />
+          </motion.div>
 
-                {dom.kategoria !== "mobilne_domy" && (
-                  <HypotekaKalkulator 
-                    cenaDoma={dom.zakladna_cena} 
-                    dom={dom}
-                    user={user}
-                    aktualnaKonfiguracia={prostoKonfiguracia}
-                    onNastavA0Prvky={() => {
-                      setIzolaciaNavysenie("premium");
-                      setTepelneCerpadlo(true);
-                      setRekuperacia(true);
-                      setProjektA0(true);
-                      setZaklady("pasove");
-                    }}
-                  />
-                )}
-                </div>
-                )}
-
+          {/* Pravý stĺpec - Informácie */}
+          <motion.div
+            initial={{ opacity: 0, x: 30 }}
+            animate={{ opacity: 1, x: 0 }}
+            className="space-y-4 sm:space-y-6 lg:self-start w-full max-w-full overflow-hidden"
+          >
                 {/* Štandardná výbava pre JAK Modules - presunute z pravej strany */}
             {isJAKModules && (
               <Card className="p-3 sm:p-4 bg-gradient-to-br from-green-50 to-white border-2 border-green-200">
