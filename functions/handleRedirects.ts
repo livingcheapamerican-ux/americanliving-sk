533b0cec170f1587868c5cafeeed1d192a3a3195
Richard Kováč <living.cheap.american@gmail.com>
2025-12-17 17:32:10 +0000

    File changes

--- functions/handleRedirects.ts
+++ functions/handleRedirects.ts
@@ -4,6 +4,10 @@ Deno.serve(async (req) => {
   
   // Mapa starých URL na nové
   const redirects = {
+    '/zna-p/ticabhouse': '/katalog?vyrobca=Ticab+house',
+    '/zna-p/ticabhouse/': '/katalog?vyrobca=Ticab+house',
+    '/zna-p/prosto-house': '/katalog?vyrobca=Prosto+House',
+    '/zna-p/prosto-house/': '/katalog?vyrobca=Prosto+House',
     '/ticab-house': '/katalog?vyrobca=Ticab+house',
     '/ticab-house/': '/katalog?vyrobca=Ticab+house',
     '/prosto-house': '/katalog?vyrobca=Prosto+House',
