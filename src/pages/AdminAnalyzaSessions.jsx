78291cf638ca8dbd9b1e94374b973e29ac7d172b
Richard Kováč <living.cheap.american@gmail.com>
2025-12-18 08:49:26 +0000

    File changes

--- src/pages/AdminAnalyzaSessions.jsx
+++ src/pages/AdminAnalyzaSessions.jsx
@@ -157,7 +157,7 @@ export default function AdminAnalyzaSessions() {
             <Globe className="w-5 h-5 mr-2" />
             <div className="text-left">
               <div className="text-xs opacity-80">Online teraz</div>
-              <div className="text-xl font-black">{stats.activeSessions}</div>
+              <div className="text-xl font-black">{onlineVisitors.count}</div>
             </div>
           </Button>
         </div>
