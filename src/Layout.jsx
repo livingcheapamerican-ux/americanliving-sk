aabc913e834be977c33f3b3cf30d4e5a6a80462e
Richard Kováč <living.cheap.american@gmail.com>
2025-12-20 16:57:00 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -109,28 +109,6 @@ import AutoTestGemini from "./components/AutoTestGemini";
   ] : [];
   const isSuperAdmin = user?.super_admin === true;
 
-  // Initialize Facebook Pixel once
-  useEffect(() => {
-    // Facebook Pixel Code
-    (function(f,b,e,v,n,t,s) {
-      if(f.fbq) return;
-      n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq) f._fbq=n;
-      n.push=n;
-      n.loaded=!0;
-      n.version='2.0';
-      n.queue=[];
-      t=b.createElement(e);
-      t.async=!0;
-      t.src=v;
-      s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s);
-    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-    window.fbq('init', '1525927175478080');
-    window.fbq('track', 'PageView');
-  }, []);
-
   // Track page views on route change
   useEffect(() => {
     if (window.fbq) {
@@ -143,6 +121,7 @@ import AutoTestGemini from "./components/AutoTestGemini";
 
   return (
     <div className="min-h-screen bg-gray-50">
+      <script dangerouslySetInnerHTML={{__html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1525927175478080');fbq('track', 'PageView');`}} />
       <noscript>
         <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=1525927175478080&ev=PageView&noscript=1" />
       </noscript>
