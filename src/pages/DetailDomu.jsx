7208a0f5c05e3f7beca306207feb4a66763cefce
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-04 06:25:25 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -34,7 +34,6 @@ import KonfiguratorTicabhouse from "../components/KonfiguratorTicabhouse";
 
 import { useLanguage } from "../components/LanguageContext";
 import TranslatedDescription from "../components/TranslatedDescription";
-import { FlyingAnimationContainer, useFlyingAnimation } from "../components/FlyingAnimation";
 
 
 export default function DetailDomu() {
@@ -107,9 +106,6 @@ export default function DetailDomu() {
   const [swipeStart, setSwipeStart] = useState(null);
   const [swipeOffset, setSwipeOffset] = useState(0);
   const [wizardKey, setWizardKey] = useState(0);
-
-  // Flying animation hook
-  const { animations, triggerAnimation } = useFlyingAnimation();
 
   // State pre Ticabhouse konfigurátor - predvolene REKREAČNÁ STAVBA (chata)
   const [lyonUcel, setLyonUcel] = useState("chata");
@@ -3310,9 +3306,6 @@ export default function DetailDomu() {
 
       {/* Floating Price Display - len ak nie je JAK Modules */}
       {!isJAKModules && <FloatingPrice price={calculatedPrice} isVisible={showCalculator} />}
-
-      {/* Flying Animations Container */}
-      <FlyingAnimationContainer animations={animations} />
 
       {/* Lightbox */}
       {lightboxOpen && (
