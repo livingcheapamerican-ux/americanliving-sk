78cb69eea98c499503430b8ee906149d71cd08bc
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-06 04:56:08 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -2441,7 +2441,7 @@ export default function DetailDomu() {
                 </div>
                 )}
 
-            {/* Floating panel pre Nord */}
+                {/* Floating panel pre Nord */}
             {isProstoHouse && dom.nazov?.includes("Nord") && (
               <div className="lg:sticky lg:top-20 z-10 self-start" style={{ position: 'sticky', top: '80px' }}>
                 <Suspense fallback={<KonfiguratorLoading />}>
