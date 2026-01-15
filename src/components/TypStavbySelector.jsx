f4297e4651dfda7bdaf72de42d2e4e7451e84455
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-15 08:44:50 +0000

    File changes

--- src/components/TypStavbySelector.jsx
+++ src/components/TypStavbySelector.jsx
@@ -65,7 +65,7 @@ export default function TypStavbySelector({
                 👇
               </motion.div>
               <p className="text-sm sm:text-base font-bold">
-                {t('clickBuildingTypeToStart') || t('clickToStartConfigurator') || 'Kliknite na typ stavby pre spustenie konfiguratora'}
+                {t('buildingTypeQuestion')}
               </p>
               <motion.div
                 animate={{ y: [0, -5, 0] }}
