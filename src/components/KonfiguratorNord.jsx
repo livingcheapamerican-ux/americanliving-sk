0c0ed38d9da2fbbdd3777d743e064dad7e60ed7c
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-22 18:46:26 +0000

    File changes

--- src/components/KonfiguratorNord.jsx
+++ src/components/KonfiguratorNord.jsx
@@ -713,7 +713,7 @@ export default function KonfiguratorNord({
                     </div>
 
                     {/* Izolácia - skupina */}
-                    <div className="col-span-1 sm:col-span-2 lg:col-span-2 grid grid-cols-2 sm:grid-cols-4 gap-1 sm:gap-2 p-1.5 sm:p-3 border-[2px] sm:border-[4px] border-cyan-600 rounded-lg sm:rounded-xl bg-cyan-100/70 shadow-xl">
+                    <div className="col-span-1 sm:col-span-3 lg:col-span-4 grid grid-cols-2 sm:grid-cols-4 gap-1 sm:gap-2 p-1.5 sm:p-3 border-[2px] sm:border-[4px] border-cyan-600 rounded-lg sm:rounded-xl bg-cyan-100/70 shadow-xl">
                       <p className="col-span-2 sm:col-span-4 text-[8px] sm:text-[10px] font-bold text-cyan-700 -mb-0.5 sm:-mb-1 flex items-center gap-0.5 sm:gap-1">
                         <span className="w-3.5 h-3.5 sm:w-5 sm:h-5 bg-cyan-600 text-white rounded-full flex items-center justify-center text-[8px] sm:text-[10px] font-extrabold">2</span>
                         {t('insulation')} ({t('selectOne')})
