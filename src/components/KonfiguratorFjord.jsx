d8df46df366e293cdcce9b153afcd3cd57739d67
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-24 21:46:50 +0000

    File changes

--- src/components/KonfiguratorFjord.jsx
+++ src/components/KonfiguratorFjord.jsx
@@ -490,7 +490,7 @@ export default function KonfiguratorFjord({
       sanitaKomplet, bojler, tepelneCerpadlo, rekuperacia, pripojkaSiete, vstupneDvere,
       stresneOkno, bocneOknoFixne, bocneOknoVyklopne90, bocneOknoVyklopne55, povrchokaOkien,
       tonovaneSkla, vonkajsiaFasada, interierFinis, vnutornePodlahy, podlahovVykurovanie,
-      interieroveDvere, inziniering, projektA0, revizna, doprava, t, BASE_PRICE]);
+      interieroveDvere, inziniering, projektA0, revizna, doprava, t, BASE_PRICE, CENY]);
 
   const [panelWidth, setPanelWidth] = useState(null);
   const [showContactModal, setShowContactModal] = useState(false);
