578bcb1031bc1f3a11a5e960cfaad96732df4988
American Living s.r.o <living.cheap.american@gmail.com>
2026-01-24 11:49:32 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -969,12 +969,12 @@ export default function DetailDomu() {
               />
             )}
 
-            {/* Konfigurátor pre Nord - vlastné ceny */}
-            {isProstoHouse && dom.nazov?.includes("Nord") && (
+            {/* Konfigurátor pre ostatné Prosto House domy */}
+            {isProstoHouse && !dom.nazov?.includes("Nord") && !dom.nazov?.includes("Fjord") && !dom.nazov?.includes("Flat 1,5") && !dom.nazov?.includes("Flat House 1,5") && !dom.nazov?.includes("Flat Double") && !dom.nazov?.includes("Flat, 72m²") && !dom.nazov?.includes("Flat Small") && !dom.nazov?.includes("A-Frame") && !dom.nazov?.includes("A-frame") && !dom.nazov?.includes("Barn") && (
               <KonfiguratorWizard
                 key={wizardKey}
                 dom={dom}
-                useNordPrices={true}
+                useProstoHousePrices={true}
                 predajNehnutelnosti={predajNehnutelnosti}
                 setPredajNehnutelnosti={setPredajNehnutelnosti}
                 hladaniePozemku={hladaniePozemku}
