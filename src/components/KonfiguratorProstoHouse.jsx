a408f70912f084f7839b71c39f40c145022e3d6e
American Living s.r.o <living.cheap.american@gmail.com>
2026-02-06 18:35:49 +0000

    File changes

--- src/components/KonfiguratorProstoHouse.jsx
+++ src/components/KonfiguratorProstoHouse.jsx
@@ -1424,7 +1424,7 @@ export default function KonfiguratorProstoHouse({
             </Card>
           </motion.div>}
 
-          {!showOnlyPhase && <motion.div
+          {!showOnlyPhase && !isAFrame && <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
