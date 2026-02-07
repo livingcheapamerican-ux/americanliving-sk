d72da076a6ba388b05e6e17714257efde0138df0
American Living s.r.o <living.cheap.american@gmail.com>
2026-02-07 00:04:24 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -1681,8 +1681,8 @@ export default function DetailDomu() {
 
 
 
-            {/* Kalkulátor hypotéky - skryté pre mobilné domy a A-Frame */}
-            {dom.kategoria !== "mobilne_domy" && !dom.nazov?.includes("A-Frame") && !dom.nazov?.includes("A-frame") && (
+            {/* Kalkulátor hypotéky - skryté pre mobilné domy, A-Frame a Prosto House */}
+            {dom.kategoria !== "mobilne_domy" && !dom.nazov?.includes("A-Frame") && !dom.nazov?.includes("A-frame") && !isProstoHouse && (
               <div className="mb-4">
                 <HypotekaKalkulator 
                   cenaDoma={dom.zakladna_cena} 
