0d52c98564493081dd9bf204837964a75b9d091e
American Living s.r.o <living.cheap.american@gmail.com>
2026-02-08 22:15:34 +0000

    File changes

--- src/pages/DetailDomu.jsx
+++ src/pages/DetailDomu.jsx
@@ -1456,7 +1456,7 @@ export default function DetailDomu() {
                 {(() => {
                   const defaultPriceMap = {
                     'PH-001': { kit: 59900, assembly: 17970 },
-                    'PH-002': { kit: 59000, assembly: 17700 },
+                    'PH-002': { kit: 59000, assembly: 19500 },
                     'PH-003': { kit: 44900, assembly: 13470 },
                     'PH-004': { kit: 49500, assembly: 14850 },
                     'PH-005': { kit: 36900, assembly: 9225 },
