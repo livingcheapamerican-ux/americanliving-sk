1f596213def46b824d6b8a656122e5799a6310d4
American Living s.r.o <living.cheap.american@gmail.com>
2026-02-08 14:10:35 +0000

    File changes

--- src/pages/KonfiguratorPH005.jsx
+++ src/pages/KonfiguratorPH005.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { base44 } from '@/api/base44Client';
+import { useQuery } from '@tanstack/react-query';
 import { 
   Home, 
   Check, 
