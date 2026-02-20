587ae5fcb828ed43a41fc7f712614a92ab5be4b4
American Living s.r.o <living.cheap.american@gmail.com>
2026-02-20 10:40:31 +0000

    File changes

--- src/pages/DotaciaAmericana.jsx
+++ src/pages/DotaciaAmericana.jsx
@@ -826,7 +826,7 @@ export default function DotaciaAmericana() {
                     <SelectContent>
                       <SelectItem value="Hotovosť">{t('dotaciaFormFinancingOption1')}</SelectItem>
                       <SelectItem value="Úver - vybavujem si sám">{t('dotaciaFormFinancingOption2')}</SelectItem>
-                      <SelectItem value="Úver vybavte mi">{t('dotaciaFormFinancingOption3')}</SelectItem>
+                      <SelectItem value="Úver vybavte mi">Vyberte mi najlepší úver</SelectItem>
                     </SelectContent>
                   </Select>
                 </div>
