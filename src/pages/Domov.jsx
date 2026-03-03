be65be5cc010e958a2483e7e50947743e3483483
American Living s.r.o <living.cheap.american@gmail.com>
2026-03-03 09:54:36 +0000

    File changes

--- src/pages/Domov.jsx
+++ src/pages/Domov.jsx
@@ -67,7 +67,7 @@ export default function Domov() {
     ? heroSettings.hero_images 
     : DEFAULT_HERO_IMAGES;
   
-  const heroInterval = heroSettings?.hero_interval || 1500;
+  const heroInterval = heroSettings?.hero_interval || 5000;
 
   useEffect(() => {
     const interval = setInterval(() => {
