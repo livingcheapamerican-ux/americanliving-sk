6c09919292d59c639e505917533332ccd553ea32
American Living s.r.o <living.cheap.american@gmail.com>
2026-03-07 12:01:42 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -44,21 +44,7 @@ function LayoutContent({ children }) {
     link.href = canonical;
   }, [location.pathname, location.search]);
 
-  // Server-Side Facebook Tracking
-  useEffect(() => {
-    const trackPageView = async () => {
-      try {
-        await base44.functions.invoke('trackFacebookPageView', {
-          user_agent: navigator.userAgent,
-          event_source_url: window.location.href
-        });
-      } catch (error) {
-        // Silently fail - tracking should not disrupt user experience
-      }
-    };
-    
-    trackPageView();
-  }, [location.pathname]);
+
 
         // GTM "Trojan Horse" Injection
         useEffect(() => {
