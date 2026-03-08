5401f071c3a55228d3061a71e5e817768c03ce67
American Living s.r.o <living.cheap.american@gmail.com>
2026-03-08 06:21:30 +0000

    File changes

--- src/pages/Marketing.jsx
+++ src/pages/Marketing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import AIMarketingChat from "../components/AIMarketingChat";
